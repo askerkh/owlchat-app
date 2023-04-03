ca1941283bdf24edbfefb9f57169eb7e3d301ea0
Asker Khuzhokov <a7ker.kh@gmail.com>
2023-04-04 00:23:32 +0300

    Configuring client for versel

--- client/src/pages/Unauthtorized.tsx
+++ client/src/pages/Unauthtorized.tsx
@@ -2,7 +2,7 @@ const Unauthtorized: React.FC = () => {
   return (
     <div className="flex w-full items-center justify-center">
       <a
-        href="http://localhost:3000/login/auth"
+        href="https://owlchat-api.onrender.com/login/auth"
         className="rounded-2xl bg-bgButton px-4 py-2 text-3xl text-iconColor transition-all duration-300 hover:rounded-sm hover:text-white"
       >
         Login with GitHub
