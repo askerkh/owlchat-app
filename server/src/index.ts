cb177da5e81a28b0062ac1da28fae725cfc6f386
Asker Khuzhokov <a7ker.kh@gmail.com>
2023-04-03 23:51:24 +0300

    Configuring server for versel

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,30 +11,6 @@ const app = express()
 
 const server = http.createServer(app)
 
-// app.get("/", (_, res) => {
-// if (req.query.token) {
-//   axios
-//     .get("https://api.github.com/user", {
-//       headers: {
-//         Authorization: `Bearer ${req.query.token}`,
-//       },
-//     })
-//     .then((res) => res.data)
-//     .then((data) => {
-//       if (data.id) {
-//         res.sendFile(path.resolve(__dirname, "../static/auth.html"))
-//       } else {
-//         res.send("<h1>You are unauthtorized</h1>")
-//       }
-//     })
-//     .catch((err) => {
-//       res.status(500).json({ message: err.message })
-//     })
-// } else {
-//   res.sendFile(path.resolve(__dirname, "../static/index.html"))
-// }
-// })
-
 app.use("/login", AuthRouter)
 
 new ChatWS(server)
